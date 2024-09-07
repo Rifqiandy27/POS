1cfe3590b8c6aa0546e56a385eaeeb6a465bc96a
Rifqiandy27 <rifqiassasin8@gmail.com>
2024-09-07 22:56:56 +0700

    Soal Praktikum - Jobsheet 2

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return "Selamat Datang";
+    }
+
+    public function homePos()
+    {
+        return view('tampilanawal_pos');
+    }
+}
